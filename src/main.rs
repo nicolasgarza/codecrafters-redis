f5b349f8fe5c9ea0b97fc4db4fed64bdac855f08
Nicolas Garza <nicolas.garza@gmail.com>
2024-05-27 17:01:23 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,73 @@
 // Uncomment this block to pass the first stage
-use std::{io::{Read, Write}, net::{TcpListener, TcpStream}, thread};
+use std::{collections::HashMap, io::{Read, Write}, net::{TcpListener, TcpStream}, thread};
+
+struct Redis {
+    data: HashMap<String, String>,
+}
+
+impl Redis {
+    fn new() -> Redis {
+        Redis {
+            data: HashMap::new(),
+        }
+    }
+
+    fn handle_client(&mut self, mut stream: TcpStream) {
+        let mut buffer = [0; 1024];
+        loop {
+            match stream.read(&mut buffer) {
+                Ok(n) => {
+                    if n == 0 { break; }
+                    let rec = String::from_utf8_lossy(&buffer[..n]);
+                    let res = self.handle_input(rec.to_string());
+                    stream.write(res.as_bytes()).unwrap();
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                    return;
+                }
+            }
+        }
+    }
+
+    fn handle_input(&mut self, input: String) -> String {
+        let words = get_words(input);
+        match words[0].as_str() {
+            "ECHO" => {
+                make_bulk_string(words[words.len() - 1].as_str())
+            },
+            "SET" => {
+                self.run_set(words[1].as_str(), words[2].as_str())
+            },
+            "GET" => {
+                self.run_get(words[1].as_str())
+            }
+            "PING" => {
+                make_simple_string("PONG")
+            },
+            _ => {
+                make_simple_string("-1")
+            }
+        }
+    }
+
+    fn run_set(&mut self, key: &str, value: &str) -> String {
+        self.data.insert(key.to_string(), value.to_string());
+        make_simple_string("OK")
+    }
+
+    fn run_get(&self, key: &str) -> String {
+        match self.data.get(key) {
+            Some(value) => {
+                make_bulk_string(value)
+            },
+            None => {
+                make_simple_string("ERROR")
+            }
+        }
+    }
+    
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -7,42 +75,12 @@ fn main() {
         match stream {
             Ok(stream) => {
                 thread::spawn(|| {
-                    handle_client(stream);
+                    let mut redis = Redis::new();
+                    redis.handle_client(stream);
                 });
             }
             Err(e) => {
                 println!("error: {}", e);
-            }
-        }
-    }
-}
-
-fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    loop {
-        match stream.read(&mut buffer) {
-            Ok(n) => {
-                let rec = String::from_utf8_lossy(&buffer[..n]);
-                println!("received: {}", rec);
-                let words = get_words(rec.to_string());
-                match words[0].as_str() {
-                    "ECHO" => {
-                        let res = make_bulk_string(words[words.len() - 1].as_str());
-                        let _ = stream.write_all(res.as_bytes());
-                    },
-                    "PING" => {
-                        let res = make_simple_string("PONG");
-                        stream.write_all(res.as_bytes()).unwrap();
-                    },
-                    _ => {
-                        let res = make_simple_string("ERROR");
-                        stream.write_all(res.as_bytes()).unwrap();
-                    }
-                }
-            }
-            Err(e) => {
-                println!("error: {}", e);
-                return;
             }
         }
     }
