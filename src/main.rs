01ac9e70ed6514167bced06c50f6e2d11a7bae0b
Nicolas Garza <nicolas.garza@gmail.com>
2024-05-26 23:40:11 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,7 @@
 use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
