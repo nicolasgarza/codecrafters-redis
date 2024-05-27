60cb484a6d431a115fc4b3f569e118bce65dac8c
Nicolas Garza <nicolas.garza@gmail.com>
2024-05-26 23:19:13 -0500

    stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +11,8 @@ fn main() {
     //
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream.write_all(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -20,3 +20,14 @@ fn main() {
         }
     }
 }
+
+// fn handle_client(mut stream: TcpStream) {
+//     let mut buffer = [0; 1024];
+//     stream.read(&mut buffer).unwrap();
+//     let rec = String::from_utf8_lossy(&buffer);
+//     if rec.starts_with("PING") {
+//         stream.write_all(b"+PONG\r\n").unwrap();
+//     } else {
+//         stream.write_all(b"-ERR unknown command\r\n").unwrap();
+//     }
+// }
